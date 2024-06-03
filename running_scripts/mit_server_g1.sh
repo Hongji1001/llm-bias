6bbdf1373d7b6526d43c25d9eeb122816e6a3a9b
pengjie <pengjieb@mail.ustc.edu.cn>
2024-06-03 15:06:06 +0800

    #

--- running_scripts/mit_server_g1.sh
+++ running_scripts/mit_server_g1.sh
@@ -2,7 +2,7 @@
  # @Author: pengjie pengjieb@mail.ustc.edu.cn
  # @Date: 2024-06-02 11:51:34
  # @LastEditors: pengjie pengjieb@mail.ustc.edu.cn
- # @LastEditTime: 2024-06-02 12:22:54
+ # @LastEditTime: 2024-06-03 15:03:10
  # @FilePath: /llm-bias/running_scripts/mit_server_g1.sh
  # @Description: 
  # 
@@ -13,4 +13,5 @@ export HOME=/data1/cache_tlc
 
 model_path=/data1/TxPLM/llm_ckpt/meta-llama/Llama-2-7b-chat-hf
 
-CUDA_VISIBLE_DEVICES=1 python generation_benchmark.py --model_name_or_path $model_path --datasets bookcorpus_new.jsonl --bias_types gender religion age race bodyshaming socioeconomic
+CUDA_VISIBLE_DEVICES=1 python llm_unlearn/unlearn_harm.py --model_name $model_path --model_save_dir checkpoint/llama2_cnn_dailymail_new_UNLEARN --datapath data/bookcorpus_new.jsonl
+# CUDA_VISIBLE_DEVICES=1 python generation_benchmark.py --model_name_or_path $model_path --datasets bookcorpus_new.jsonl --bias_types gender religion age race bodyshaming socioeconomic
--- running_scripts/mit_server_g2.sh
+++ running_scripts/mit_server_g2.sh
@@ -2,8 +2,8 @@
  # @Author: pengjie pengjieb@mail.ustc.edu.cn
  # @Date: 2024-06-02 11:51:34
  # @LastEditors: pengjie pengjieb@mail.ustc.edu.cn
- # @LastEditTime: 2024-06-02 11:58:48
- # @FilePath: /llm-bias/running_scripts/mit_server_g3.sh
+ # @LastEditTime: 2024-06-03 15:05:28
+ # @FilePath: /llm-bias/running_scripts/mit_server_g2.sh
  # @Description: 
  # 
  # Copyright (c) 2024 by ${git_name_email}, All Rights Reserved. 
@@ -11,5 +11,6 @@
 export TRANSFORMERS_CACHE="/data1/tianlong/.cache"
 export HOME=/data1/cache_tlc
 
+CUDA_VISIBLE_DEVICES=2 python llm_unlearn/unlearn_harm.py --model_name $model_path --model_save_dir models/llama2_unlearned --log_file logs/llama2-unlearn.log
 
-CUDA_VISIBLE_DEVICES=2 python generation_benchmark.py --model_name_or_path xlnet/xlnet-base-cased --datasets bookcorpus_new.jsonl
+# CUDA_VISIBLE_DEVICES=2 python generation_benchmark.py --model_name_or_path xlnet/xlnet-base-cased --datasets bookcorpus_new.jsonl
