44cb21ec6a90b19869c4609a3c855d3cc0c60412
pengjie <pengjieb@mail.ustc.edu.cn>
2024-06-05 12:39:35 +0800

    #

--- running_scripts/debias_network_training_g3.sh
+++ running_scripts/debias_network_training_g3.sh
@@ -3,7 +3,7 @@
  # @Author: pengjie pengjieb@mail.ustc.edu.cn
  # @Date: 2024-06-05 12:22:00
  # @LastEditors: pengjie pengjieb@mail.ustc.edu.cn
- # @LastEditTime: 2024-06-05 12:24:57
+ # @LastEditTime: 2024-06-05 12:39:25
  # @FilePath: /llm-bias/running_scripts/debias_network_training_g3.sh
  # @Description: 
  # 
@@ -11,4 +11,4 @@
 ### 
 model_path=/data1/TxPLM/llm_ckpt/facebook/opt-1.3b
 
-CUDA_VISIBLE_DEVICES=1 python llm-unlearning/unlearn_harm.py --model_name $model_path --model_save_dir checkpoint/opt_bookcorpus_new_UNLEARN --datapath data/bookcorpus_new.jsonl --use_lora
+CUDA_VISIBLE_DEVICES=3 python llm-unlearning/unlearn_harm.py --model_name $model_path --model_save_dir checkpoint/opt_bookcorpus_new_UNLEARN --datapath data/bookcorpus_new.jsonl --use_lora
