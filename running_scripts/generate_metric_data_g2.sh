d0561bd9b09de3ba774a615b879f1028e5ac915d
pengjie <pengjieb@mail.ustc.edu.cn>
2024-06-06 05:29:31 +0800

    #

--- running_scripts/generate_metric_data_g2.sh
+++ running_scripts/generate_metric_data_g2.sh
@@ -2,7 +2,7 @@
  # @Author: pengjie pengjieb@mail.ustc.edu.cn
  # @Date: 2024-06-05 11:22:05
  # @LastEditors: pengjie pengjieb@mail.ustc.edu.cn
- # @LastEditTime: 2024-06-06 05:18:16
+ # @LastEditTime: 2024-06-06 05:29:22
  # @FilePath: /llm-bias/running_scripts/generate_metric_data_g2.sh
  # @Description: 
  # 
@@ -22,6 +22,6 @@ model_path=/data1/TxPLM/llm_ckpt/meta-llama/Llama-2-7b-chat-hf
 # CUDA_VISIBLE_DEVICES=2 python generation_benchmark_no_metric.py --model_name_or_path /data1/TxPLM/llm-bias/checkpoint/llama2_cnn_dailymail_new_UNLEARN  --datasets bold.jsonl
 # CUDA_VISIBLE_DEVICES=2 python generation_benchmark_no_metric.py --model_name_or_path /data1/TxPLM/llm-bias/checkpoint/llama2_cnn_dailymail_new_UNLEARN  --datasets bookcorpus_new.jsonl
 
-CUDA_VISIBLE_DEVICES=2 python generation_benchmark_no_metric.py --model_name_or_path /data1/TxPLM/llm-bias/checkpoint/llama2_unlearned  --datasets cnn_dailymail_new.jsonl bodyshame socioeconomic lgbt appearance class education disability national
+CUDA_VISIBLE_DEVICES=2 python generation_benchmark_no_metric.py --model_name_or_path /data1/TxPLM/llm-bias/checkpoint/llama2_unlearned  --datasets cnn_dailymail_new.jsonl --bias_types bodyshame socioeconomic lgbt appearance class education disability national
 CUDA_VISIBLE_DEVICES=2 python generation_benchmark_no_metric.py --model_name_or_path /data1/TxPLM/llm-bias/checkpoint/llama2_unlearned  --datasets bold.jsonl
 CUDA_VISIBLE_DEVICES=2 python generation_benchmark_no_metric.py --model_name_or_path /data1/TxPLM/llm-bias/checkpoint/llama2_unlearned  --datasets bookcorpus_new.jsonl --bias_types socioeconomic lgbt appearance class education disability national
