c469d18e28b7f47294d27f7009ec8986d51ca082
pengjie <pengjieb@mail.ustc.edu.cn>
2024-06-11 09:18:33 +0800

    #

--- running_scripts/debias_network_training_g1.sh
+++ running_scripts/debias_network_training_g1.sh
@@ -6,6 +6,6 @@ model_path=/data1/TxPLM/llm_ckpt/meta-llama/Meta-Llama-3-8B-instruct
 
 
 
-CUDA_VISIBLE_DEVICES=1 python llm-unlearning/unlearn_harm.py --model_name meta-llama/Meta-Llama-3-8B-instruct $model_path --model_save_dir checkpoint/llama3 --log_file logs/llama3-unlearn.log --use_lora
+CUDA_VISIBLE_DEVICES=1 python llm-unlearning/unlearn_harm.py --model_name $model_path --model_save_dir checkpoint/llama3 --log_file logs/llama3-unlearn.log --use_lora
 
 # CUDA_VISIBLE_DEVICES=1 python llm-unlearning/unlearn_harm.py --model_name $model_path --model_save_dir checkpoint/opt_unlearned --log_file logs/opt-unlearn.log --use_lora
