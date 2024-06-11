4df1b20344f8add9ac0263cece79a451a4e57e25
pengjie <pengjieb@mail.ustc.edu.cn>
2024-06-11 09:13:20 +0800

    new command

--- running_scripts/debias_network_training_g1.sh
+++ running_scripts/debias_network_training_g1.sh
@@ -2,6 +2,10 @@
 export TRANSFORMERS_CACHE="/data1/tianlong/.cache"
 export HOME=/data1/cache_tlc
 
-model_path=/data1/TxPLM/llm_ckpt/facebook/opt-1.3b
+model_path=/data1/TxPLM/llm_ckpt/meta-llama/Meta-Llama-3-8B-instruct
 
-CUDA_VISIBLE_DEVICES=1 python llm-unlearning/unlearn_harm.py --model_name $model_path --model_save_dir checkpoint/opt_unlearned --log_file logs/opt-unlearn.log --use_lora
+
+
+CUDA_VISIBLE_DEVICES=1 python llm-unlearning/unlearn_harm.py --model_name meta-llama/Meta-Llama-3-8B-instruct $model_path --model_save_dir checkpoint/llama3 --log_file logs/llama3-unlearn.log --use_lora
+
+# CUDA_VISIBLE_DEVICES=1 python llm-unlearning/unlearn_harm.py --model_name $model_path --model_save_dir checkpoint/opt_unlearned --log_file logs/opt-unlearn.log --use_lora
